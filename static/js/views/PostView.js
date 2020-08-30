70b15da238caffb6bf5a0ca4487f8e51150e6132
Michael Currin <michael.currin@gmail.com>
2020-08-30 15:35:17 +0200

    feat: Update pages

--- static/js/views/PostView.js
+++ static/js/views/PostView.js
@@ -3,14 +3,15 @@ import AbstractView from './AbstractView.js';
 export default class extends AbstractView {
   constructor(params) {
     super(params);
+    const { id } = params;
+    this.postID = id;
     this.setTitle('Viewing Post');
   }
 
   async getHtml() {
     return `
         <h1>Post</h1>
-        <p>You are viewing a post!</p>
+        <p>You are viewing post #${this.postID}. No content can be shown here yet.</p>
     `;
-    Í;
   }
 }
--- static/js/views/Posts.js
+++ static/js/views/Posts.js
@@ -7,9 +7,18 @@ export default class extends AbstractView {
   }
 
   async getHtml() {
+    // This is hardcoded for now but could be done dynamically using templating
+    // and maybe some posts in a posts folder.
     return `
       <h1>Posts</h1>
-      <p>You are viewing the posts!</p>
+      <ul>
+        <li>
+          <a href="/posts/1" data-link>#1 - How to build a Node.js app</a>
+        </li>
+        <li>
+          <a href="/posts/2" data-link>#2 - How to build a GH Pages site</a>
+        </li>
+      </ul>
     `;
   }
 }
