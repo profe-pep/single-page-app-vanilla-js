65901cfd6487e8b1607c1328263e217fff487d7d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-14 00:52:13 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,12 @@ const pathToRegex = path =>
 
 const getParams = match => {
   const values = match.result.slice(1);
-  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+  const keys = Array.from(
+    match.route.path
+      .matchAll(/:(\w+)/g))
+      .map(result => result[1]
+  );
 
   return Object.fromEntries(
     keys.map((key, i) => {
@@ -34,7 +39,7 @@ const router = async () => {
     { path: u('/settings'), view: Settings }
   ];
 
-  // Test each route for potential match
+  // Test each route for a potential match.
   const potentialMatches = routes.map(route => {
     return {
       route: route,
